a77213c1d237e96e9498a15104b9a5979814c0fe
Melik <melikozy@gmail.com>
2021-01-20 15:07:13 +0300

    String utils

---
+++ app/src/main/java/com/appbroker/livetvplayer/util/StringUtils.java
@@ -0,0 +1,40 @@
+package com.appbroker.livetvplayer.util;
+
+import android.net.Uri;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class StringUtils {
+    public static String randomString(int size) {
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(size);
+
+        for (int i = 0; i < size; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
+    public static Uri makeUri(String url){
+        try {
+            return Uri.parse(URLEncoder.encode(url, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+}
